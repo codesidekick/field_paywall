652deebbc02e834969995cd31cc3ad8f1651cd1a
Marton Bodonyi <marty@interactivejunky.com>
2015-03-31 14:26:52 +1100

    Added unit test for formatter settings form.

--- src/Tests/Unit/FieldPaywallFieldFormatterUnitTest.php
+++ src/Tests/Unit/FieldPaywallFieldFormatterUnitTest.php
@@ -9,6 +9,7 @@ namespace Drupal\field_paywall\Tests\Unit;
 
 use Drupal\Core\Field\FieldItemListInterface;
 use Drupal\Core\Field\FieldItemInterface;
+use Drupal\Core\Form\FormState;
 use Drupal\field\Tests\FieldUnitTestBase;
 use Drupal\field_paywall\Plugin\Field\FieldFormatter\PaywallFormatter;
 use Drupal\Core\Language\LanguageInterface;
@@ -25,6 +26,11 @@ class FieldPaywallFieldFormatterUnitTest extends FieldUnitTestBase {
 
   protected $paywallHiddenFields = array();
 
+  protected $otherFieldNames = array(
+    'field_test_1',
+    'field_test_2',
+  );
+
   /**
    * The paywall formatter plugin to test.
    *
@@ -38,35 +44,13 @@ class FieldPaywallFieldFormatterUnitTest extends FieldUnitTestBase {
   protected function setUp() {
     parent::setUp();
 
-    // Create a boolean field and storage for validation.
-    entity_create('field_storage_config', array(
-      'field_name' => 'field_paywall',
-      'entity_type' => 'entity_test',
-      'type' => 'paywall',
-    ))->save();
-    entity_create('field_config', array(
-      'entity_type' => 'entity_test',
-      'field_name' => 'field_paywall',
-      'bundle' => 'entity_test',
-    ))->save();
+    $this->createPaywallField();
 
-    $formatter_plugin_manager = \Drupal::service('plugin.manager.field.formatter');
+    foreach ($this->otherFieldNames as $field_name) {
+      $this->createBasicTextField($field_name);
+    }
 
-    $entity_manager = $this->container->get('entity.manager');
-    $definitions = $entity_manager->getFieldDefinitions('entity_test', 'entity_test');
-    $formatter_options = array(
-      'field_definition' => $definitions['field_paywall'],
-      'view_mode' => 'default',
-      'configuration' => array(
-        'type' => 'paywall_formatter',
-        'settings' => array(
-          'message' => $this->paywallTestMessage,
-          'hidden_fields' => $this->paywallHiddenFields,
-        ),
-      ),
-    );
-
-    $this->paywallFormatterInstance = $formatter_plugin_manager->getInstance($formatter_options);
+    $this->setFormatterInstance();
   }
 
   /**
@@ -114,8 +98,21 @@ class FieldPaywallFieldFormatterUnitTest extends FieldUnitTestBase {
    * @covers ::settingsForm
    */
   public function testSettingsForm() {
-//    $form_state = $this->getMock('Drupal\Core\Form\FormStateInterface');
-//    $settings_form = $this->paywallFormatterInstance->settingsForm(array(), $form_state);
+    $form_state = new FormState();
+    $settings_form = $this->paywallFormatterInstance->settingsForm(array(), $form_state);
+
+    $this->assertEqual('textarea', $settings_form['message']['#type'], 'Message field is a textarea');
+    $this->assertEqual('checkboxes', $settings_form['hidden_fields']['#type'], 'Hidden fields field is checkboxes');
+
+    $this->assertEqual($this->paywallTestMessage, $settings_form['message']['#default_value'], 'Message default value in settings form correct');
+    $this->assertEqual($this->paywallHiddenFields, $settings_form['hidden_fields']['#default_value'], 'Hidden fields default value in settings form correct');
+
+    // Check that hidden fields is showing availables correctly.
+    $hidden_field_options = $settings_form['hidden_fields']['#options'];
+    $this->assertEqual(count($this->otherFieldNames), count($hidden_field_options), 'Correct number of hidden field options');
+    foreach ($settings_form['hidden_fields']['#options'] as $field_name) {
+      $this->assertTrue(in_array($field_name, $this->otherFieldNames), 'Field name option correct');
+    }
   }
 //
 //  /**
@@ -158,6 +155,70 @@ class FieldPaywallFieldFormatterUnitTest extends FieldUnitTestBase {
     $entity->save();
 
     return $entity;
+  }
+
+  /**
+   * Create the paywall field.
+   */
+  protected function createPaywallField() {
+    entity_create('field_storage_config', array(
+      'field_name' => 'field_paywall',
+      'entity_type' => 'entity_test',
+      'type' => 'paywall',
+    ))->save();
+    entity_create('field_config', array(
+      'entity_type' => 'entity_test',
+      'field_name' => 'field_paywall',
+      'bundle' => 'entity_test',
+    ))->save();
+  }
+
+  /**
+   * Create a basic string textfield and attach to the entity bundle.
+   *
+   * @param string $field_name
+   *   The field name to create.
+   */
+  protected function createBasicTextField($field_name) {
+    entity_create('field_storage_config', array(
+      'field_name' => $field_name,
+      'entity_type' => 'entity_test',
+      'type' => 'string',
+      'cardinality' => 1,
+    ))->save();
+
+    entity_create('field_config', array(
+      'entity_type' => 'entity_test',
+      'field_name' => $field_name,
+      'bundle' => 'entity_test',
+    ))->save();
+
+    entity_get_display('entity_test', 'entity_test', 'default')
+      ->setComponent($field_name)
+      ->save();
+  }
+
+  /**
+   * Set the formatter instance used in the test.
+   */
+  protected function setFormatterInstance() {
+    $formatter_plugin_manager = \Drupal::service('plugin.manager.field.formatter');
+
+    $entity_manager = $this->container->get('entity.manager');
+    $definitions = $entity_manager->getFieldDefinitions('entity_test', 'entity_test');
+    $formatter_options = array(
+      'field_definition' => $definitions['field_paywall'],
+      'view_mode' => 'default',
+      'configuration' => array(
+        'type' => 'paywall_formatter',
+        'settings' => array(
+          'message' => $this->paywallTestMessage,
+          'hidden_fields' => $this->paywallHiddenFields,
+        ),
+      ),
+    );
+
+    $this->paywallFormatterInstance = $formatter_plugin_manager->getInstance($formatter_options);
   }
 
 }
